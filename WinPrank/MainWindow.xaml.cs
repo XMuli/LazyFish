193ceaeb83b066e5d7fe9d89f9aa321d9b73b339
XMuli <xmulitech@gmail.com>
2025-02-07 15:24:02 +0800

    feat: using NavigationView

--- WinPrank/MainWindow.xaml.cs
+++ WinPrank/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WinPrank.Pages;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -28,9 +29,45 @@ namespace WinPrank
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+
+        private void nav_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
         {
-            myButton.Content = "Clicked";
+            var selectedItem = nav.SelectedItem as NavigationViewItem;
+            if (selectedItem == null) return;
+
+            string? selectedTag = selectedItem.Tag?.ToString();
+            switch (selectedTag)
+            {
+                case "WindowsCrashPage":
+                    if (m_windowsCrashPage is null)
+                    {
+                        m_windowsCrashPage = new WindowsCrashPage();
+                    }
+
+                    navContentFrame.Content = m_windowsCrashPage;
+                    break;
+                case "WindowsUpdatePage":
+                    if (m_windowsUpdatePage is null)
+                    {
+                        m_windowsUpdatePage = new WindowsUpdatePage();
+                    }
+
+                    navContentFrame.Content = m_windowsUpdatePage;
+                    break;
+                case "informationPage":
+                    if (m_informationPages is null)
+                    {
+                        m_informationPages = new InformationPages();
+                    }
+                    navContentFrame.Content = m_informationPages;
+                    break;
+                default: break;
+            }
         }
+
+
+        private WindowsCrashPage  m_windowsCrashPage;
+        private WindowsUpdatePage m_windowsUpdatePage;
+        private InformationPages  m_informationPages;
     }
 }
